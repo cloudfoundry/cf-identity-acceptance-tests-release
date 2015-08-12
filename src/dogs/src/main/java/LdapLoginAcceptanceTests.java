7e17514c6a95d72f0f1ac59000d6c34d0b461b23
Jonathan Lo <jlo@us.ibm.com>
2015-08-12 15:15:27 -0700

    Use an ldap user
    Signed-off-by: Madhura Bhave <mbhave@pivotal.io>

--- src/dogs/src/main/java/LdapLoginAcceptanceTests.java
+++ src/dogs/src/main/java/LdapLoginAcceptanceTests.java
@@ -46,8 +46,8 @@ public class LdapLoginAcceptanceTests {
 
     @Test
     public void ldap_login_IsSuccessful() {
-        webDriver.findElement(By.name("username")).sendKeys("marissa");
-        webDriver.findElement(By.name("password")).sendKeys("koala");
+        webDriver.findElement(By.name("username")).sendKeys("marissa3");
+        webDriver.findElement(By.name("password")).sendKeys("ldap3");
         webDriver.findElement(By.xpath("//input[@value='Sign in']")).click();
 
         assertThat(webDriver.findElement(By.cssSelector("h1")).getText(), Matchers.containsString("Where to?"));
