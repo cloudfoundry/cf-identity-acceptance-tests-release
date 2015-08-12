c6b666f4246ab0cf6468e9314e55f2122d164a7f
Madhura Bhave <mbhave@pivotal.io>
2015-08-12 15:09:33 -0700

    Add ldap tests
    [#100463252] https://www.pivotaltracker.com/story/show/100463252
    
    Signed-off-by: Jonathan Lo <jlo@us.ibm.com>

---
+++ src/dogs/src/main/java/LdapLoginAcceptanceTests.java
@@ -0,0 +1,59 @@
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.assertThat;
+
+/*******************************************************************************
+ * Cloud Foundry
+ * Copyright (c) [2009-2015] Pivotal Software, Inc. All Rights Reserved.
+ * <p>
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ * <p>
+ * This product includes a number of subcomponents with
+ * separate copyright notices and license terms. Your use of these
+ * subcomponents is subject to the terms and conditions of the
+ * subcomponent's license, as noted in the LICENSE file.
+ *******************************************************************************/
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DefaultAcceptanceTestConfig.class)
+public class LdapLoginAcceptanceTests {
+
+
+    @Value("${BASE_URL}")
+    private String baseUrl;
+
+    @Value("${PROTOCOL}")
+    private String protocol;
+
+    @Autowired
+    private WebDriver webDriver;
+
+    @Before
+    @After
+    public void clearWebDriverOfCookies() throws Exception {
+        webDriver.get(protocol + baseUrl + "/logout.do");
+    }
+
+    @Test
+    public void ldap_login_IsSuccessful() {
+        webDriver.findElement(By.name("username")).sendKeys("marissa");
+        webDriver.findElement(By.name("password")).sendKeys("koala");
+        webDriver.findElement(By.xpath("//input[@value='Sign in']")).click();
+
+        assertThat(webDriver.findElement(By.cssSelector("h1")).getText(), Matchers.containsString("Where to?"));
+
+        webDriver.get(protocol + baseUrl + "/logout.do");
+
+        assertThat(webDriver.findElement(By.cssSelector("h1")).getText(), Matchers.containsString("Welcome!"));
+    }
+}
