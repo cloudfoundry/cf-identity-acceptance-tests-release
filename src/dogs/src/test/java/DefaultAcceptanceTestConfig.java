ab751eca8e102cce51283f67101b203b25017dc4
Peter Chen <peterch@vmware.com>
2022-08-08 12:15:28 -0700

    Fix: various phantom-js related failures
    - errors:
    ```
    org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'webDriver' defined in DefaultAcceptanceTestConfig
    ```
    and:
    ```
    org.openqa.selenium.UnsupportedCommandException: TypeError - undefined is not a function (evaluating '_getTagName(currWindow).toLowerCase()')
    ```
    - This commit resolves these issues by replacing
    phantom-js with ChromeDriver, as research has shown that
    phantom-js has fallen out of standards and is not maintained
    well (see: https://github.com/ariya/phantomjs/issues/15344).
    ChromeDriver is now the standard (also what `uaa` repo uses)
    - the driver config is based on the existing configs in `uaa`
    repo, which has tests of very similar nature as this repo:
    https://github.com/cloudfoundry/uaa/blob/d3cbf284ae92fc4cfb6c68f908c479f5e5b22e9e/uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/DefaultIntegrationTestConfig.java#L53
    
    [#182917624]

--- src/dogs/src/test/java/DefaultAcceptanceTestConfig.java
+++ src/dogs/src/test/java/DefaultAcceptanceTestConfig.java
@@ -10,11 +10,9 @@
  *     subcomponents is subject to the terms and conditions of the
  *     subcomponent's license, as noted in the LICENSE file.
  *******************************************************************************/
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +20,6 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class DefaultAcceptanceTestConfig {
@@ -33,13 +30,24 @@ public class DefaultAcceptanceTestConfig {
 
     @Bean(destroyMethod = "quit")
     public WebDriver webDriver() {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {"--web-security=no", "--ignore-ssl-errors=yes"});
-        PhantomJSDriver driver = new PhantomJSDriver(desiredCapabilities);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-        driver.manage().timeouts().setScriptTimeout(15, TimeUnit.SECONDS);
-        driver.manage().window().setSize(new Dimension(1024, 768));
+        System.setProperty("webdriver.chrome.logfile", "/tmp/chromedriver.log");
+        System.setProperty("webdriver.chrome.verboseLogging", "true");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "--verbose",
+                "--headless",
+                "--disable-web-security",
+                "--ignore-certificate-errors",
+                "--allow-running-insecure-content",
+                "--allow-insecure-localhost",
+                "--no-sandbox",
+                "--disable-gpu"
+        );
+
+        options.setAcceptInsecureCerts(true);
+
+        ChromeDriver driver = new ChromeDriver(options);
         return driver;
     }
 
